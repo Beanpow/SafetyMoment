335bfaa75f0e7067379db5d2cbb51e4d3773dc96
beanpow <760823780@qq.com>
2021-12-04 18:51:05 +0800

    change default sampleRate and add autoPlot
    change default sampleRate from 6 to 20 because of the update of hardware
    add autoPlot

--- SafetyMomentManager.py
+++ SafetyMomentManager.py
@@ -3,15 +3,17 @@ import numpy as np
 from AngleManager import AngleManager
 import os
 from MomentManager import MomentManager
+import matplotlib.pyplot as plt
 
 class SafetyMomentManager:
-    def __init__(self, userName, sampleRate=4) -> None:
+    def __init__(self, userName, sampleRate=20, autoPlot = False) -> None:
         '''
         userName:       The User Name
         sampleRate:     Hz, the value must less than 4, because of the momentManager have the limited rate
         '''
         self.userName = userName
         self.sampleRate = sampleRate
+        self.autoPlot = autoPlot
         self.GetSafetyMoment()
 
     def GetSafetyMoment(self):
@@ -50,7 +52,11 @@ class SafetyMomentManager:
         angleData = []
 
         try:
+            if self.autoPlot:
+                plt.figure(1)
             while(1):
+                if self.autoPlot:
+                    plt.clf()
                 startTime = time.time()
                 moment = momentManager.GetAllMoments()
                 #angle = angelManager.GetAllMoment()
@@ -62,14 +68,19 @@ class SafetyMomentManager:
                 print(endTime - startTime)
                 assert(endTime - startTime <= timeDuration)
 
+                print('time stop: ', timeDuration - (endTime - startTime))
+
                 time.sleep(timeDuration - (endTime - startTime))
+                if self.autoPlot:
+                    plt.plot(momentData[-100:])
+                    plt.pause(0.01)
 
         except KeyboardInterrupt:
             print("End the Record!")
          
-        np.save('rawdata.npy', momentData)
+        np.save('rawdata1.npy', momentData)
         return momentData, angleData
 
 
 if __name__ == "__main__":
-    smm = SafetyMomentManager('hkb', sampleRate=6)
+    smm = SafetyMomentManager('hkb', sampleRate=40, autoPlot = True)
