bda112148545949bcb44ddd55bc5be755d23b4ab
beanpow <760823780@qq.com>
2021-12-04 18:47:24 +0800

    remove * 10

--- demo.py
+++ demo.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-a = np.load('rawdata copy 4.npy')
+a = np.load('rawdata1.npy')
 print(a)
 plt.plot(a[:, 0])
 plt.plot(a[:, 1])
-plt.plot(a[:, 2] * 10)
+plt.plot(a[:, 2])
 plt.plot(a[:, 3])
 plt.legend(['1','2','3','4'])
 plt.show()
