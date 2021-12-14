065adcf84e656eca446f3f583a3e8bbcf7c36b78
beanpow <760823780@qq.com>
2021-12-14 14:40:52 +0800

    move gait gen to the folder gait gen

--- gait_gen/test.py
+++ gait_gen/test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 angleList = []
 
-with open('./data/gait.csv', mode='r', encoding='utf-8-sig') as f:
+with open('./gait_gen/gait.csv', mode='r', encoding='utf-8-sig') as f:
     for line in f:
         line = line.split('\n')[0].split(',')
         line = [float(i) for i in line]
@@ -13,16 +13,16 @@ with open('./data/gait.csv', mode='r', encoding='utf-8-sig') as f:
 angleList = np.array(angleList)
 print(angleList.shape)
 
-if 0:
+if 1:
     plt.plot(angleList[:, 0], label='hl')
     plt.plot(angleList[:, 1], label='kl')
-    plt.plot(angleList[:, 2], label='hr')
-    plt.plot(angleList[:, 3], label='kr')
+    # plt.plot(angleList[:, 2], label='hr')
+    # plt.plot(angleList[:, 3], label='kr')
     plt.legend()
     plt.show()
 
-with open('./data/gait_predict.csv', mode='w', encoding='utf-8-sig') as f:
+with open('./gait_gen/gait_predict.csv', mode='w', encoding='utf-8-sig') as f:
     for i in range(angleList.shape[0]):
-        f.write(str(angleList[i, 3] / 2) + ', ')
+        f.write(str(angleList[i, 1] / 2) + ', ')
     f.write('\n')
     
